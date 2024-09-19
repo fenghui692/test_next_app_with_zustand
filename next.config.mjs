7bef73b7113d33bce22d2b4ff324d147c2c749f5
fenghui <fenghui692@gmail.com>
2024-09-20 00:51:47 +0800

    chore: generate next static page

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  output: "export",
+  basePath: "/test_next_app_with_zustand/out",
 };
 
 export default nextConfig;
